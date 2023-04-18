311a6db8409ce75bb6cfd5b2627731afe8404014
Dwayne <dwayne@tornmarketing.com.au>
2023-04-19 01:42:23 +1000

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -12,8 +12,8 @@
 return [
     'name'        => 'Plivo',
     'description' => 'Plivo Sms integration (australian tweak)',
-    'author'      => 'mtcextendee.com',
-    'version'     => '1.0.0',
+    'author'      => 'tornmarketing.com.au',
+    'version'     => '1.2.0',
     'services' => [
         'events'  => [],
         'forms'   => [
@@ -23,11 +23,9 @@ return [
             'mautic.sms.transport.plivo' => [
                 'class'     => \MauticPlugin\MauticPlivoBundle\Services\PlivoApi::class,
                 'arguments' => [
-                    'mautic.page.model.trackable',
-                    'mautic.helper.phone_number',
                     'mautic.helper.integration',
                     'monolog.logger.mautic',
-                    'mautic.http.connector'
+                    'mautic.http.client'
                 ],
                 'alias' => 'mautic.sms.config.transport.plivo',
                 'tag'          => 'mautic.sms_transport',
@@ -40,11 +38,46 @@ return [
         'integrations' => [
             'mautic.integration.plivo' => [
                 'class' => \MauticPlugin\MauticPlivoBundle\Integration\PlivoIntegration::class,
+            'arguments' => [
+                    'event_dispatcher',
+                    'mautic.helper.cache_storage',
+                    'doctrine.orm.entity_manager',
+                    'session',
+                    'request_stack',
+                    'router',
+                    'translator',
+                    'logger',
+                    'mautic.helper.encryption',
+                    'mautic.lead.model.lead',
+                    'mautic.lead.model.company',
+                    'mautic.helper.paths',
+                    'mautic.core.model.notification',
+                    'mautic.lead.model.field',
+                    'mautic.plugin.model.integration_entity',
+                    'mautic.lead.model.dnc',
+                ],
             ],
         ],
     ],
     'routes'     => [],
-    'menu'       => [],
-    'parameters' => [
+    'menu'       => [
+        'main' => [
+            'items' => [
+                'mautic.sms.smses' => [
+                    'route'    => 'mautic_sms_index',
+                    'access'   => ['sms:smses:viewown', 'sms:smses:viewother'],
+                    'parent'   => 'mautic.core.channels',
+                    'checks'   => [
+                        'integration' => [
+                            'Mtalkz' => [
+                                'enabled' => true,
+                            ],
+                        ],
+                    ],
+                    'priority' => 70,
+                ],
+            ],
+        ],
     ],
+    'parameters' => [],
 ];
